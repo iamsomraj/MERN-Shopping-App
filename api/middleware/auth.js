6ecdb4ea75a26aac68c5ea50a95c058cd60995e5
iamsomraj <iamsomraj@gmail.com>
2020-09-30 18:18:59 +0530

    auth middleware fixed

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-	const token = req.header("x-auth-token");
+	let token = req.header("authorization");
+
+	token = token.slice(7, token.length);
 
 	if (!token) {
 		return res.status(401).json({ msg: "No token, authorization denied" });
