2751a7db39c973d7d3c41c80213289fcb3e1c1c2
iamsomraj <iamsomraj@gmail.com>
2020-09-30 18:17:17 +0530

    profile delete route added

--- api/routes/profile.routes.js
+++ api/routes/profile.routes.js
@@ -83,18 +83,19 @@ router.post(
 );
 
 // @route    DELETE api/profile
-// @desc     Delete profile, user & posts
+// @desc     Delete profile, user & orders, products
 // @access   Private
 router.delete("/", auth, async (req, res) => {
 	try {
-		// Remove user posts
-		await Post.deleteMany({ user: req.user.id });
+		// @TODO: remove orders
+		// @TODO: remove products
+
 		// Remove profile
 		await Profile.findOneAndRemove({ user: req.user.id });
 		// Remove user
 		await User.findOneAndRemove({ _id: req.user.id });
 
-		res.json({ msg: "User deleted" });
+		res.json({ msg: "Account deleted" });
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send("Server Error");
