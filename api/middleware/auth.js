80fed6e28a25e80d54b276d7e427e4b534918f43
iamsomraj <iamsomraj@gmail.com>
2020-09-30 12:46:36 +0530

    cb removed

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -9,17 +9,10 @@ module.exports = (req, res, next) => {
 	}
 
 	try {
-		jwt.verify(token, process.env.SECRET, (error, decoded) => {
-			if (error) {
-				return res
-					.status(401)
-					.json({ errors: [{ msg: "Token is not valid" }] });
-			} else {
-				req.user = decoded.user;
-				next();
-			}
-		});
+		const decoded = jwt.verify(token, process.env.SECRET);
+		req.user = decoded.user;
+		next();
 	} catch (err) {
-		res.status(500).json({ errors: [{ msg: "Internal Server Error" }] });
+		res.status(401).json({ errors: [{ msg: "Token is invalid" }] });
 	}
 };
