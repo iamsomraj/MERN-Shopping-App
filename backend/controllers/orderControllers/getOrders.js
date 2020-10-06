69d2fc5ec0693a07e3df44011070b79b3c015f9a
iamsomraj <iamsomraj@gmail.com>
2020-10-06 17:25:44 +0530

    get all orders added

---
+++ backend/controllers/orderControllers/getOrders.js
@@ -0,0 +1,12 @@
+import Order from "../../models/Order.js";
+import asyncHandler from "express-async-handler";
+
+// @desc:   Get all orders
+// @access: private
+// @route:  /api/orders
+const getOrders = asyncHandler(async (req, res) => {
+	const orders = await Order.find().populate("user", "id name");
+	res.json(orders);
+});
+
+export default getOrders;
--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,11 +1,14 @@
 import express from "express";
+import getOrders from "../controllers/orderControllers/getOrders.js";
 import payOrder from "../controllers/orderControllers/payOrder.js";
 import placeOrder from "../controllers/orderControllers/placeOrder.js";
-import { userAuth } from "../middlewares/auth.js";
+import { adminAuth, userAuth } from "../middlewares/auth.js";
 
 const Router = express.Router();
 
-Router.route("/").post(userAuth, placeOrder);
+Router.route("/")
+	.post(userAuth, placeOrder)
+	.get(userAuth, adminAuth, getOrders);
 Router.route("/:id").put(userAuth, payOrder);
 
 export default Router;
