57b5a3a9763ad70dfe43e76f7e14d79f97a18d7d
iamsomraj <iamsomraj@gmail.com>
2020-10-06 16:27:17 +0530

    get product by id route added

---
+++ backend/controllers/productControllers/getProduct.js
@@ -0,0 +1,19 @@
+import Product from "../../models/Product.js";
+import asyncHandler from "express-async-handler";
+
+// @desc:   get one product by id
+// @access: public
+// @route:  api/products/:id
+
+const getProduct = asyncHandler(async (req, res) => {
+	const product = await Product.findById(req.params.id);
+	if (product) {
+		res.status(200).json(product);
+	} else {
+		const message = "Product is unavailable";
+		res.status(404).json({ message });
+		throw new Error(message);
+	}
+});
+
+export default getProduct;
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import createProduct from "../controllers/productControllers/createProduct.js";
 import deleteProduct from "../controllers/productControllers/deleteProduct.js";
+import getProduct from "../controllers/productControllers/getProduct.js";
 import getProducts from "../controllers/productControllers/getProducts.js";
 import upload from "../controllers/productControllers/uploadImage.js";
 import { adminAuth, userAuth } from "../middlewares/auth.js";
@@ -10,6 +11,6 @@ const Router = express.Router();
 Router.route("/")
 	.get(getProducts)
 	.post(userAuth, adminAuth, upload.single("image"), createProduct);
-Router.route("/:id").delete(userAuth, adminAuth, deleteProduct);
+Router.route("/:id").get(getProduct).delete(userAuth, adminAuth, deleteProduct);
 
 export default Router;
