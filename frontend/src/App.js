4b15c0ebbfdf0ee9abd77c775ed46f2607412887
iamsomraj <iamsomraj@gmail.com>
2020-10-08 18:34:38 +0530

    landing page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "./components/layout/Navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "reactstrap";
+import Landing from "./components/layout/Landing";
 
 const App = () => {
 	return (
 		<div>
-			<Navbar />
-			App component
+			<Router>
+				<Navbar />
+				<Container>
+					<Landing />
+				</Container>
+			</Router>
 		</div>
 	);
 };
--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navbar, NavbarToggler, Collapse, NavItem, Nav } from "reactstrap";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
-	return <div>Nav</div>;
+const NavBar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleHandler = () => {
+		setIsOpen(!isOpen);
+	};
+
+	return (
+		<div>
+			<Navbar light expand="md">
+				<Link className="navbar-brand" to="/">
+					coding pillow shop
+				</Link>
+				<NavbarToggler onClick={toggleHandler}></NavbarToggler>
+				<Collapse isOpen={isOpen} navbar>
+					<Nav navbar className="ml-auto">
+						<NavItem>
+							<Link className="nav-link" to="/">
+								Products
+							</Link>
+						</NavItem>
+						<NavItem>
+							<Link className="nav-link" to="/register">
+								Register
+							</Link>
+						</NavItem>
+						<NavItem>
+							<Link className="nav-link" to="/login">
+								Login
+							</Link>
+						</NavItem>
+					</Nav>
+				</Collapse>
+			</Navbar>
+		</div>
+	);
 };
 
-export default Navbar;
+export default NavBar;
