cf8e64a7916406757a355b4d5b0f225376f16110
iamsomraj <iamsomraj@gmail.com>
2020-10-10 12:08:21 +0530

    Frontend cleaned

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,5 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Container } from "reactstrap";
-import Login from "./Layout/Login";
-import Register from "./Layout/Register";
-import Landing from "./Layout/Landing";
-import Navbar from "./Layout/NavbarLayout";
-import store from "./redux/store";
-
 const App = () => {
-  return (
-    <Provider store={store}>
-      <Router>
-        <Navbar />
-        <Container className="mt-4">
-          <Switch>
-            <Route path="/" exact component={Landing} />
-            <Route path="/login" exact component={Login} />
-            <Route path="/register" exact component={Register} />
-          </Switch>
-        </Container>
-      </Router>
-    </Provider>
-  );
+  return <div>This is our application</div>;
 };
 
 export default App;
