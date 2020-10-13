52a676129a4dafbed432f8f1042b8d56b2404890
iamsomraj <iamsomraj@gmail.com>
2020-10-13 15:47:58 +0530

    oder layout fixed

--- frontend/src/components/Order.js
+++ frontend/src/components/Order.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, Col, ListGroup, Row, Table } from "react-bootstrap";
-import { PayPalButton } from "react-paypal-button-v2";
 import Loader from "./Loader";
+import { PayPalButton } from "react-paypal-button-v2";
 
 const Order = (props) => {
   const { order, onPay, loading, ready } = props;
@@ -11,72 +11,78 @@ const Order = (props) => {
       <Card.Header>Order #{order._id}</Card.Header>
       <Card.Body>
         <Card.Title>Total Price - ${order.totalPrice.toFixed(2)}</Card.Title>
-        <ListGroup>
-          <ListGroup.Item>
-            <Row>
-              <Col>Name</Col>
-              <Col>{order.user.name}</Col>
-            </Row>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Row>
-              <Col>Contact</Col>
-              <Col>{order.user.email}</Col>
-            </Row>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Row>
-              <Col>Payment Status</Col>
-              <Col>
-                {order.isPaymentDone ? (
-                  <strong className="text text-success">Successful</strong>
-                ) : (
-                  <strong className="text text-danger">Pending</strong>
-                )}
-              </Col>
-            </Row>
-          </ListGroup.Item>
-        </ListGroup>
-        <Table>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Quanity</th>
-              <th>Price</th>
-              <th>Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            {order.products.map((item, index) => (
-              <tr key={item._id}>
-                <th scope="row">{index + 1}</th>
-                <td>{item.name}</td>
-                <td>{item.qty}</td>
-                <td>{item.price.toFixed(2)}</td>
-                <td>{(item.qty * item.price).toFixed(2)}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-        <Row className="justify-content-center">
+        <Row>
           <Col md={8}>
+            <ListGroup>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Name</Col>
+                  <Col>{order.user.name}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Contact</Col>
+                  <Col>{order.user.email}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Payment Status</Col>
+                  <Col>
+                    {order.isPaymentDone ? (
+                      <strong className="text text-success">Successful</strong>
+                    ) : (
+                      <strong className="text text-danger">Pending</strong>
+                    )}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={4}>
             {!order.isPaymentDone && (
-              <ListGroup>
-                <ListGroup.Item>
-                  {loading && <Loader />}
-                  {!ready ? (
-                    <Loader />
-                  ) : (
-                    <PayPalButton
-                      amount={order.totalPrice}
-                      onSuccess={(result) => onPay(order._id)}
-                    />
-                  )}
-                </ListGroup.Item>
-              </ListGroup>
+              <div>
+                {loading && <Loader />}
+                {!ready ? (
+                  <Loader />
+                ) : (
+                  <PayPalButton
+                    amount={order.totalPrice}
+                    style={{
+                      shape: "pill",
+                    }}
+                    onSuccess={(result) => onPay(order._id)}
+                  />
+                )}
+              </div>
             )}
           </Col>
+        </Row>
+        <Row className="m-2">
+          <Card.Title>Order Details</Card.Title>
+          <Table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Quanity</th>
+                <th>Price</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {order.products.map((item, index) => (
+                <tr key={item._id}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{item.name}</td>
+                  <td>{item.qty}</td>
+                  <td>{item.price.toFixed(2)}</td>
+                  <td>{(item.qty * item.price).toFixed(2)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
         </Row>
       </Card.Body>
       <Card.Footer className="text-muted">
