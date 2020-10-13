760f0c96f88119a40923f344c2c66975059638a6
iamsomraj <iamsomraj@gmail.com>
2020-10-13 20:37:51 +0530

    added filtering own account

--- backend/controllers/adminControllers/getAllUsersByAdmin.js
+++ backend/controllers/adminControllers/getAllUsersByAdmin.js
@@ -5,8 +5,12 @@ import asyncHandler from "express-async-handler";
 // @access  private
 // @route   GET /api/users
 const getAllUsers = asyncHandler(async (req, res) => {
-	const users = await User.find().select();
-	res.status(200).json(users);
+  const users = await User.find({
+    _id: {
+      $ne: req.user._id,
+    },
+  });
+  res.status(200).json(users);
 });
 
 export default getAllUsers;
--- backend/data/users.js
+++ backend/data/users.js
@@ -1,22 +1,28 @@
 import bcrypt from "bcryptjs";
 
 const users = [
-	{
-		name: "Admin User",
-		email: "admin@example.com",
-		password: bcrypt.hashSync("123456", 10),
-		isAdmin: true,
-	},
-	{
-		name: "John Doe",
-		email: "john@example.com",
-		password: bcrypt.hashSync("123456", 10),
-	},
-	{
-		name: "Jane Doe",
-		email: "jane@example.com",
-		password: bcrypt.hashSync("123456", 10),
-	},
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "John Doe",
+    email: "john@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Jane Doe",
+    email: "jane@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Somraj Mukherjee",
+    email: "somraj@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
 ];
 
 export default users;
