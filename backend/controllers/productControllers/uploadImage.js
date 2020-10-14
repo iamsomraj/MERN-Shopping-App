34bf90c2055516bf5186aaf68cf79f6d3e825f47
iamsomraj <iamsomraj@gmail.com>
2020-10-14 13:47:29 +0530

    image file name updated

--- backend/controllers/productControllers/uploadImage.js
+++ backend/controllers/productControllers/uploadImage.js
@@ -1,12 +1,11 @@
 import multer from "multer";
-import asyncHandler from "express-async-handler";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, req.user.id + "-" + Date.now() + ".jpg");
+    cb(null, req.user.id + "-" + Date.now() + "-" + file.originalname);
   },
 });
 
