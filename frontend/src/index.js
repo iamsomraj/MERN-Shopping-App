9a8a229720c482ffb074220d73c4cb2255487a9e
Somraj Mukherjee <iamsomraj@gmail.com>
2020-10-16 23:06:32 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,6 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-
-// main app component
 ReactDOM.render(
   <Provider store={store}>
     <App />
