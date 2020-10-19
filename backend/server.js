a8ba96f99f2555d96ea39de7d725fa68bbb9769b
iamsomraj <iamsomraj@gmail.com>
2020-10-19 12:59:30 +0530

    added fix for server in production mode

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,10 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  // @TODO : Handle Requests
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
   app.get("/", (req, res) => {
     res.send("Our Express API is running..");
