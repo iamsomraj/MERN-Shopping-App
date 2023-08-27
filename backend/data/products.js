1297b162e1290cc2ad323b04d813eb56b9f306d1
Somraj Mukherjee <iamsomraj@gmail.com>
2023-08-27 21:20:26 +0530

    📦 NEW: added product data

---
+++ backend/data/products.js
@@ -0,0 +1,123 @@
+const products = [
+  {
+    name: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+    image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    price: 119.99,
+    qtyInStock: 50,
+  },
+  {
+    name: 'Mens Casual Premium Slim Fit T-Shirts',
+    image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+    price: 24.99,
+    qtyInStock: 100,
+  },
+  {
+    name: 'Mens Cotton Jacket',
+    image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+    price: 59.99,
+    qtyInStock: 80,
+  },
+  {
+    name: 'Mens Casual Slim Fit',
+    image: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
+    price: 18.99,
+    qtyInStock: 70,
+  },
+  {
+    name: "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+    image: 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg',
+    price: 699.99,
+    qtyInStock: 30,
+  },
+  {
+    name: 'Solid Gold Petite Micropave',
+    image: 'https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg',
+    price: 179.99,
+    qtyInStock: 20,
+  },
+  {
+    name: 'White Gold Plated Princess',
+    image: 'https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg',
+    price: 12.99,
+    qtyInStock: 150,
+  },
+  {
+    name: 'Pierced Owl Rose Gold Plated Stainless Steel Double',
+    image: 'https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg',
+    price: 14.99,
+    qtyInStock: 200,
+  },
+  {
+    name: 'WD 2TB Elements Portable External Hard Drive - USB 3.0',
+    image: 'https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg',
+    price: 69.99,
+    qtyInStock: 60,
+  },
+  {
+    name: 'SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s',
+    image: 'https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg',
+    price: 119.99,
+    qtyInStock: 40,
+  },
+  {
+    name: 'Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5',
+    image: 'https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg',
+    price: 129.99,
+    qtyInStock: 50,
+  },
+  {
+    name: 'WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive',
+    image: 'https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg',
+    price: 139.99,
+    qtyInStock: 30,
+  },
+  {
+    name: 'Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin',
+    image: 'https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg',
+    price: 629.99,
+    qtyInStock: 25,
+  },
+  {
+    name: 'Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED',
+    image: 'https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg',
+    price: 1049.99,
+    qtyInStock: 15,
+  },
+  {
+    name: "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
+    image: 'https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg',
+    price: 69.99,
+    qtyInStock: 75,
+  },
+  {
+    name: "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
+    image: 'https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg',
+    price: 39.99,
+    qtyInStock: 100,
+  },
+  {
+    name: 'Rain Jacket Women Windbreaker Striped Climbing Raincoats',
+    image: 'https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg',
+    price: 49.99,
+    qtyInStock: 90,
+  },
+  {
+    name: "MBJ Women's Solid Short Sleeve Boat Neck V",
+    image: 'https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg',
+    price: 19.99,
+    qtyInStock: 120,
+  },
+  {
+    name: "Opna Women's Short Sleeve Moisture",
+    image: 'https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg',
+    price: 14.99,
+    qtyInStock: 150,
+  },
+  {
+    name: 'DANVOUY Womens T Shirt Casual Cotton Short',
+    image: 'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg',
+    price: 16.99,
+    qtyInStock: 110,
+  },
+];
+export default products;
