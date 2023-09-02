566c983ce930a8829747381d3b6efbc3f00abceb
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-02 16:39:54 +0530

    👌 IMPROVE: added basic layout structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,15 @@ const App = () => {
   return (
     <>
       <Header />
+      {/* BEGIN - MAIN CONTENT */}
       <main className='py-24'>
         <section className='container py-6'>
+          {/* SCREEN CONTENT */}
           <Outlet />
+          {/* SCREEN CONTENT */}
         </section>
       </main>
+      {/* END - MAIN CONTENT */}
     </>
   );
 };
