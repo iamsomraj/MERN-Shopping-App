578c324a99e81f690e87977bbaac8592a0af0b5d
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-07 23:37:28 +0530

    👌 IMPROVE: cleaned up single product component

--- frontend/src/components/SingleProductScreen/Product.tsx
+++ frontend/src/components/SingleProductScreen/Product.tsx
@@ -1,5 +1,4 @@
 import { IProduct } from '@/types';
-import { Link } from 'react-router-dom';
 
 type Props = {
   product: IProduct;
@@ -7,26 +6,17 @@ type Props = {
 
 const Product = ({ product }: Props) => {
   return (
-    <div className='group bg-zinc-50 md:flex rounded-xl border overflow-hidden'>
-      <div className='bg-zinc-100 dark:bg-zinc-300 duration-300 transition-all p-6 flex justify-center items-center'>
+    <>
+      <div>
         <img
-          className='h-60 w-60 object-cover mix-blend-multiply object-center group-hover:scale-110 transition-all duration-300'
+          title='h-60 w-60 object-cover mix-blend-multiply object-center transition-all duration-300'
           src={product.image}
         />
+        <h3 title='text-4xl font-medium text-zinc-900 line-clamp-1 dark:text-zinc-50 group-hover:underline'>{product.name}</h3>
+        <h3 title='text-xl line-through font-medium text-red-500'>${(product.price * 1.5).toFixed(2)}</h3>
+        <h3 title='text-6xl font-thin text-zinc-500'>${product.price}</h3>
       </div>
-      <div className='p-6 flex flex-col md:flex-1 bg-zinc-50 dark:bg-zinc-800 gap-6 md:group-hover:px-9 transition-all duration-300'>
-        <Link
-          key={product._id}
-          to={`/products/${product._id}`}>
-          <h3 className='text-4xl font-medium text-zinc-900 line-clamp-1 dark:text-zinc-50 group-hover:underline'>{product.name}</h3>
-        </Link>
-        <div className='text-2xl font-medium text-zinc-500'>Price</div>
-        <div className='flex gap-6 items-center'>
-          <h3 className='text-xl line-through font-medium text-red-500'>${(product.price * 1.5).toFixed(2)}</h3>
-          <h3 className='text-6xl font-thin text-zinc-500'>${product.price}</h3>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
