e17520fb3aa949e94c1959495e695c082b895b03
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-08 14:01:22 +0530

    👌 IMPROVE: added close btn and prod item

--- frontend/src/components/Cart/CartDrawer.tsx
+++ frontend/src/components/Cart/CartDrawer.tsx
@@ -29,19 +29,25 @@ const CartDrawer = () => {
   };
 
   const drawerContent = (
-    <div
-      className={`fixed z-20 min-h-screen max-h-screen w-full inset-0 md:inset-y-0 md:left-auto md:right-0 md:w-1/2 md:rounded-l-lg bg-zinc-100 dark:bg-zinc-800 flex justify-center items-center transition-all duration-300 drop-shadow-xl transform ${
-        showDrawer ? 'translate-x-0' : 'translate-x-full'
-      }`}>
-      <div className='absolute top-5 right-5'>
-        <Button onClick={onToggle}>
-          <div className='flex justify-center items-center gap-1'>
-            <XMarkIcon className='h-5 w-5 flex-shrink-0' />
-            <span className='hidden md:block'>Close</span>
-          </div>
-        </Button>
+    <>
+      {/* BEGIN - CONTENT CONTAINER */}
+      <div
+        className={`fixed z-20 min-h-screen max-h-screen w-full inset-0 md:inset-y-0 md:left-auto md:right-0 md:w-1/2 md:rounded-l-lg bg-zinc-100 dark:bg-zinc-800 flex justify-center items-center transition-all duration-300 drop-shadow-xl transform ${
+          showDrawer ? 'translate-x-0' : 'translate-x-full'
+        }`}>
+        {/* BEGIN - CLOSE BUTTON */}
+        <div className='absolute top-5 right-5'>
+          <Button onClick={onToggle}>
+            <div className='flex justify-center items-center gap-3'>
+              <XMarkIcon className='h-5 w-5 flex-shrink-0' />
+              <span className='hidden md:block'>Close</span>
+            </div>
+          </Button>
+        </div>
+        {/* END - CLOSE BUTTON */}
       </div>
-    </div>
+      {/* END - CONTENT CONTAINER */}
+    </>
   );
 
   return drawerContent;
--- frontend/src/components/ProductScreen/ProductListItem.tsx
+++ frontend/src/components/ProductScreen/ProductListItem.tsx
@@ -8,7 +8,7 @@ type Props = {
 const ProductListItem = ({ product }: Props) => {
   return (
     <div className='group bg-zinc-50 md:flex rounded-xl border overflow-hidden'>
-      <div className='bg-zinc-100 dark:bg-zinc-300 duration-300 transition-all p-6 flex justify-center items-center'>
+      <div className='h-60 w-full md:w-60 flex-shrink-0 bg-zinc-100 dark:bg-zinc-300 duration-300 transition-all p-6 flex justify-center items-center'>
         <img
           className='h-60 w-60 object-cover mix-blend-multiply object-center group-hover:scale-110 transition-all duration-300'
           src={product.image}
@@ -20,11 +20,8 @@ const ProductListItem = ({ product }: Props) => {
           to={`/products/${product._id}`}>
           <h3 className='text-4xl font-medium text-zinc-900 line-clamp-1 dark:text-zinc-50 group-hover:underline'>{product.name}</h3>
         </Link>
-        <div className='text-2xl font-medium text-zinc-500'>Price</div>
-        <div className='flex gap-6 items-center'>
-          <h3 className='text-xl line-through font-medium text-red-500'>${(product.price * 1.5).toFixed(2)}</h3>
-          <h3 className='text-6xl font-thin text-zinc-500'>${product.price}</h3>
-        </div>
+        <h3 className='text-xl line-through font-medium text-red-500'>${(product.price * 1.5).toFixed(2)}</h3>
+        <h3 className='text-6xl font-thin text-zinc-500'>${product.price}</h3>
       </div>
     </div>
   );
