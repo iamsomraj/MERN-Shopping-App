2ebb045c21336455e15c614b276ac8879fb1373f
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-10 02:02:04 +0530

    👌 IMPROVE: edited payment wrapper

--- frontend/src/components/PaymentScreen/PaymentWrapper.tsx
+++ frontend/src/components/PaymentScreen/PaymentWrapper.tsx
@@ -95,7 +95,7 @@ const PayementWrapper = () => {
     isPaypalConfigLoading || paypalConfigError || !paypalClientId ? (
       loadingContent
     ) : (
-      <div className='mx-auto max-w-xs'>
+      <div className='w-fit z-0'>
         <PayPalScriptProvider options={{ clientId: paypalClientId }}>
           <PayPalButtons
             style={{ shape: 'pill' }}
@@ -110,21 +110,42 @@ const PayementWrapper = () => {
   const orderContent = (
     <article
       key={order._id}
-      className='rounded-xl w-full bg-zinc-50 dark:bg-zinc-800 overflow-hidden border drop-shadow'>
-      <div className='p-6 bg-zinc-200 dark:bg-zinc-700 text-3xl font-medium'>Payment Details</div>
-      <div className='p-6 flex flex-col gap-3'>
-        <p className='font-medium text-lg'>Total Items - {order.products.length}</p>
-        <p className='text-2xl font-medium'>$ {order.totalPrice.toFixed(2)}</p>
-        <div className='grid grid-cols-5 gap-3 text-sm font-semibold text-zinc-500 dark:text-zinc-400'>
-          {order.products.map((prod, index) => (
-            <>
-              <span className='col-span-5 md:col-span-1'>Product {index + 1}</span>
-              <span className='col-span-5 md:col-span-1'>{prod.name}</span>
-              <span className='col-span-5 md:col-span-1'>{prod.qty}</span>
-              <span className='col-span-5 md:col-span-1'>{prod.price}</span>
-              <span className='col-span-5 md:col-span-1'>{(prod.qty * prod.price).toFixed(2)}</span>
-            </>
+      className='rounded-lg flex flex-col gap-3 w-full'>
+      <h1 className='text-3xl font-semibold'>
+        Order <span className='text-sm italic opacity-50 font-medium'>#{order._id}</span>
+      </h1>
+      <div className='flex flex-col gap-3'>
+        <h3 className='text-lg font-semibold dark'>Items Overview</h3>
+        <ul className=''>
+          {order.products.map((prod) => (
+            <li
+              key={prod._id}
+              className='flex flex-wrap justify-between items-center py-2 gap-6 border-b-2 border-b-zinc-500/10'>
+              <h4 className='text-base font-medium'>{prod.name}</h4>
+              <div className='flex items-center gap-3'>
+                <p className='text-xs font-medium'>${prod.price.toFixed(2)}</p>
+                <span>x</span>
+                <p className='text-xs font-medium'>{prod.qty}</p>
+                <span>=</span>
+                <p className='text-xs font-medium'>${(prod.qty * prod.price).toFixed(2)}</p>
+              </div>
+            </li>
           ))}
+        </ul>
+      </div>
+      <div>
+        <p className='text-lg font-semibold'>Total Price</p>
+        <p className='opacity-50 font-medium'>${order.totalPrice.toFixed(2)}</p>
+        <p className='text-lg font-semibold'>Payment Status</p>
+        <p className='opacity-50 font-medium'>{order.isPaymentDone ? 'Paid' : 'Pending'}</p>
+      </div>
+      <hr className='border-b' />
+      <div>
+        <h3 className='text-lg font-semibold'>User Information</h3>
+        <div className='flex gap-3 text-sm'>
+          <p className='font-medium opacity-50'>{order.user.name}</p>
+          <span>-</span>
+          <p className='font-medium opacity-50'>{order.user.email}</p>
         </div>
       </div>
     </article>
@@ -135,7 +156,7 @@ const PayementWrapper = () => {
       {isPaymentInProgress ? (
         loadingContent
       ) : (
-        <div className='flex flex-col gap-12 md:flex-row items-start'>
+        <div className='flex flex-col gap-12 md:flex-row items-start md:justify-between'>
           {orderContent}
           {paypalContent}
         </div>
