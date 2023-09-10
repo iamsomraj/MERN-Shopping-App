c4b500525433b9f9824e5998f37504c421bcc285
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-10 13:50:21 +0530

    🐛 FIX: cart drawer fix

--- frontend/src/components/Cart/CartDrawer.tsx
+++ frontend/src/components/Cart/CartDrawer.tsx
@@ -96,80 +96,78 @@ const CartDrawer = () => {
       {/* END - DRAWER OVERLAY */}
 
       {/* BEGIN - CART CONTAINER */}
-      <div className={`fixed z-20 min-h-screen max-h-screen w-full inset-0 md:inset-y-0 md:left-auto md:right-0 md:w-1/2 md:rounded-l-lg bg-zinc-100 dark:bg-zinc-800 transition-all duration-300 drop-shadow-xl transform ${showDrawer ? 'translate-x-0' : 'translate-x-full'}`}>
-        <div className='flex flex-col w-full h-full'>
-          {/* BEGIN - CART HEADER */}
-          <div className='flex justify-between items-center px-6 py-3 border-b border-b-zinc-200 dark:border-b-zinc-700 drop-shadow-lg'>
-            {/* BEGIN - HEADING */}
-            <h3 className='text-2xl text-zinc-900 dark:text-zinc-100 font-medium'>
-              Cart Items
-              {cart.length > 0 && <span className='ml-3'>({cart.length})</span>}
-            </h3>
-            {/* END - HEADING */}
+      <div className={`fixed z-20 flex flex-col min-h-screen max-h-screen w-full inset-0 md:inset-y-0 md:left-auto md:right-0 md:w-1/2 md:rounded-l-lg bg-zinc-100 dark:bg-zinc-800 transition-all duration-300 drop-shadow-xl transform ${showDrawer ? 'translate-x-0' : 'translate-x-full'}`}>
+        {/* BEGIN - CART HEADER */}
+        <div className='flex justify-between items-center px-6 py-3 border-b border-b-zinc-200 dark:border-b-zinc-700 drop-shadow-lg'>
+          {/* BEGIN - HEADING */}
+          <h3 className='text-2xl text-zinc-900 dark:text-zinc-100 font-medium'>
+            Cart Items
+            {cart.length > 0 && <span className='ml-3'>({cart.length})</span>}
+          </h3>
+          {/* END - HEADING */}
 
-            {/* BEGIN - CLOSE BUTTON */}
-            <Button onClick={onToggle}>
-              <div className='flex justify-center items-center gap-3'>
-                <XMarkIcon className='h-5 w-5 flex-shrink-0' />
-                <span className='hidden md:block'>Close</span>
-              </div>
-            </Button>
-            {/* END - CLOSE BUTTON */}
-          </div>
-          {/* END - CART HEADER */}
+          {/* BEGIN - CLOSE BUTTON */}
+          <Button onClick={onToggle}>
+            <div className='flex justify-center items-center gap-3'>
+              <XMarkIcon className='h-5 w-5 flex-shrink-0' />
+              <span className='hidden md:block'>Close</span>
+            </div>
+          </Button>
+          {/* END - CLOSE BUTTON */}
+        </div>
+        {/* END - CART HEADER */}
 
-          {/* BEGIN - CART BODY */}
-          <div className='flex-1 overflow-y-auto'>
-            {cart.map((product) => (
-              <CartItem
-                key={product._id}
-                product={product}
-                deleteSingleItem={deleteSingleItem}
-                redirectToProduct={navigateToProductPage}
-              />
-            ))}
-          </div>
-          {/* END - CART BODY  */}
+        {/* BEGIN - CART BODY */}
+        <div className='flex-1 overflow-y-auto'>
+          {cart.map((product) => (
+            <CartItem
+              key={product._id}
+              product={product}
+              deleteSingleItem={deleteSingleItem}
+              redirectToProduct={navigateToProductPage}
+            />
+          ))}
+        </div>
+        {/* END - CART BODY  */}
 
-          {/* BEGIN - CART FOOTER  */}
-          <div className='flex justify-between items-center px-6 py-3 border-t border-t-zinc-200 dark:border-t-zinc-700 drop-shadow-lg'>
-            {cart.length > 0 ? (
-              <>
-                {/* BEGIN - CLEAR CART BUTTON */}
-                <Button
-                  variant='transparent'
-                  onClick={deleteCartItems}>
-                  <div className='flex justify-center items-center gap-3'>
-                    <TrashIcon className='h-5 w-5 flex-shrink-0' />
-                    <span className='hidden md:block'>Clear Cart</span>
-                  </div>
-                </Button>
-                {/* END - CLEAR CART BUTTON */}
-              </>
-            ) : (
-              <span className='text-base text-zinc-900 font-bold dark:text-zinc-50 duration-300 transition-all'>No Items In Cart</span>
-            )}
-
-            <div className='flex justify-end items-center gap-3 md:gap-6'>
-              {/* BEGIN - TOTAL PRICE */}
-              {cart.length > 0 && <span className='text-base md:text-2xl font-medium dark:text-zinc-100'>$ {total}</span>}
-              {/* END - TOTAL PRICE */}
-
-              {/* BEGIN - PLACE ORDER BUTTON */}
+        {/* BEGIN - CART FOOTER  */}
+        <div className='bottom-0 flex justify-between items-center px-6 py-3 border-t border-t-zinc-200 dark:border-t-zinc-700 drop-shadow-lg'>
+          {cart.length > 0 ? (
+            <>
+              {/* BEGIN - CLEAR CART BUTTON */}
               <Button
-                disabled={user === null}
-                onClick={() => placeOrder()}
-                loading={isLoading}>
+                variant='transparent'
+                onClick={deleteCartItems}>
                 <div className='flex justify-center items-center gap-3'>
-                  {user !== null ? <ForwardIcon className='h-5 w-5 flex-shrink-0' /> : <LockClosedIcon className='h-5 w-5 flex-shrink-0' />}
-                  <span>Place Order</span>
+                  <TrashIcon className='h-5 w-5 flex-shrink-0' />
+                  <span className='hidden md:block'>Clear Cart</span>
                 </div>
               </Button>
-              {/* END - PLACE ORDER BUTTON */}
-            </div>
+              {/* END - CLEAR CART BUTTON */}
+            </>
+          ) : (
+            <span className='text-base text-zinc-900 font-bold dark:text-zinc-50 duration-300 transition-all'>No Items In Cart</span>
+          )}
+
+          <div className='flex justify-end items-center gap-3 md:gap-6'>
+            {/* BEGIN - TOTAL PRICE */}
+            {cart.length > 0 && <span className='text-base md:text-2xl font-medium dark:text-zinc-100'>$ {total}</span>}
+            {/* END - TOTAL PRICE */}
+
+            {/* BEGIN - PLACE ORDER BUTTON */}
+            <Button
+              disabled={user === null}
+              onClick={() => placeOrder()}
+              loading={isLoading}>
+              <div className='flex justify-center items-center gap-3'>
+                {user !== null ? <ForwardIcon className='h-5 w-5 flex-shrink-0' /> : <LockClosedIcon className='h-5 w-5 flex-shrink-0' />}
+                <span>Place Order</span>
+              </div>
+            </Button>
+            {/* END - PLACE ORDER BUTTON */}
           </div>
-          {/* END - CART FOOTER  */}
         </div>
+        {/* END - CART FOOTER  */}
       </div>
       {/* END - CART CONTAINER */}
     </>
