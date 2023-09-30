743a1647a7a7e176f5069c4cc457eb3255d9ce19
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-30 13:41:18 +0530

    👌 IMPROVE: added data fetching on the inventory page

--- frontend/src/components/InventoryScreen/InventoryWrapper.tsx
+++ frontend/src/components/InventoryScreen/InventoryWrapper.tsx
@@ -1,5 +1,40 @@
+import { getProducts } from '@/api/products';
+import { getErrorMessage } from '@/config';
+import { useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { useLocation } from 'react-router-dom';
+
 const InventoryWrapper = () => {
-  return <div>InventoryWrapper</div>;
+  const [page, setPage] = useState<number>(1);
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const pageNumberFromQuery = queryParams.get('page');
+
+  useEffect(() => {
+    if (pageNumberFromQuery) {
+      setPage(Number(pageNumberFromQuery));
+    }
+  }, [pageNumberFromQuery]);
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: [`inventory-${page}-products`],
+    queryFn: async () => {
+      return await getProducts(page);
+    },
+    onError: (error) => {
+      const errorMessage = getErrorMessage(error, 'Error occurred while fetching inventory details!');
+      toast.error(errorMessage);
+    },
+  });
+
+  if (isLoading) return 'Loading...';
+
+  if (error) return 'Error occurred while fetching inventory details!';
+
+  const content = <div className='flex flex-col gap-12'>{JSON.stringify(data)}</div>;
+
+  return content;
 };
 
 export default InventoryWrapper;
